ed41c297b293e74637d16f95f36a4a3ec089b2a0
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-07-15 14:25:06 +0200

    Factory design pattern + Singleton Pattern

---
+++ Assignment2_Question4/registration.cpp
@@ -0,0 +1,35 @@
+#include "registration.h"
+#include <QString>
+
+Registration::Registration(Person a) : m_Attendee(a), STANDARD_FEE(500),
+    m_BookingDate(QDate::currentDate())
+{
+}
+
+QDate Registration::getBookingDate()
+{
+    return m_BookingDate;
+}
+
+Person Registration::getAttendee()
+{
+    return m_Attendee;
+}
+
+double Registration::calculateFee()
+{
+    return STANDARD_FEE;
+}
+
+QString Registration::toString()
+{
+    return QString("%1 %2 ").arg(m_Attendee.toString()).arg(m_BookingDate.toString("dd.MM.yyyy"));
+}
+
+void Registration::setBookingDate(const QDate newBookingDate)
+{
+    m_BookingDate = newBookingDate;
+}
+
+
+
---
+++ Assignment2_Question4/registration.h
@@ -0,0 +1,26 @@
+#ifndef REGISTRATION_H
+#define REGISTRATION_H
+
+#include "person.h"
+#include <QDate>
+
+class QString;
+
+class Registration : public QObject
+{
+    Q_OBJECT
+public:
+    Registration(Person a);
+    Person getAttendee();
+    QDate getBookingDate();
+    double calculateFee();
+    QString toString();
+    double STANDARD_FEE;
+    void setBookingDate(const QDate newBookingDate);
+
+private:
+    Person m_Attendee;
+    QDate m_BookingDate;
+};
+
+#endif // REGISTRATION_H
