7fbeda2bb0743a8b51ef1c1467d006339c532f5d
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-07-19 14:14:31 +0200

    Removed Comments

--- Assignment2_Question4_Complete/main.cpp
+++ Assignment2_Question4_Complete/main.cpp
@@ -1,19 +1,10 @@
 #include <QApplication>
 #include <QDebug>
 #include "registrationdialog.h"
-#include "registrationfactory.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    // RegistrationFactory *creator = RegistrationFactory::GetInstance();
-    // Person p("ash", "ash@gmaill.com", "ut");
-    // Person p1("ashs", "ash@gmaill.com", "ut");
-    // Person p2("ashg", "ash@gmaill.com", "ut");
-    // qDebug() << creator->FactoryMethod("Registration",p, "")->metaObject()->className() << "\n";
-    // qDebug() << creator->FactoryMethod("StudentRegistration",p1, "BSc Comp SCi")->metaObject()->className() << "\n";
-    // qDebug() << creator->FactoryMethod("GuestRegistration",p2, "Visitor")->metaObject()->className() << "\n";
 
 
     RegistrationDialog dialog;
