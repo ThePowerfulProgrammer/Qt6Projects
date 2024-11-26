5c1e39b5c5854ae6202ac1e3b809114a9c05c5fd
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-11-26 12:41:47 +0200

    Done with arkanoid v1.0

---
+++ RaylibDev/BasicBuild/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_BasicBuild_FILE C:/Qt6 Projects/RaylibDev/BasicBuild/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/BasicBuild.exe)
+set(__QT_DEPLOY_TARGET_BasicBuild_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_BasicBuild_RUNTIME_DLLS C:/Qt6/6.7.2/mingw_64/bin/Qt6Core.dll)
--- RaylibDev/BasicBuild/main.cpp
+++ RaylibDev/BasicBuild/main.cpp
@@ -2,106 +2,252 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <vector>
+
+
 
 using namespace std;
 
+const int screenWidth = 900;
+const int screenHeight = 720;
 
+struct Paddle
+{
+    Vector2 position;
+    Vector2 size;
+    Color color;
+};
 
-int main(void)
+struct Ball
+{
+    Vector2 position;
+    Vector2 speed;
+    float r;
+    Color c;
+};
+
+struct Brick
+{
+    Vector2 position;
+    Vector2 size;
+    bool active;
+
+    Brick()
+    {
+        position.x = 0;
+        position.y = 0;
+
+        size.x = 0;
+        size.y = 0;
+
+    };
+
+    Brick(float x, float y, float length,float width, bool act=true)
+    {
+        position.x = x;
+        position.y = y;
+
+        size.x = length;
+        size.y = width;
+
+        active = act;
+    }
+};
+
+const int rows = 2;
+const int cols = 20;
+
+Brick bricks[rows][cols]; // bricks array[rows][cols]:
+Paddle paddle;
+Ball ball;
+
+// init the bricks,paddle and ball before the first draw
+void startGame()
 {
 
-    const int screenWidth = 700;
-    const int screenHeight = 720;
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    float startingX = 0;
+    float startingY = 40;
 
-    Color colors[] = {DARKGRAY, GRAY, DARKGRAY, GRAY, DARKGRAY, GRAY};
-    vector<Color> blockColors;
-    srand(time(0));
+    float l = 45;
+    float w = 45;
 
-    for (int i=0;i<28;i++)
+    for (int i=0;i<2;i++)
     {
-        blockColors.push_back(colors[rand() %(5-0+1)]);
+        for (int j=0;j<20;j++)
+        {
+            Brick brick(startingX, startingY, l, w);
+            bricks[i][j] = brick;
+            startingX += 45;
+        }
+        startingX = 0;
+        startingY += 45;
     }
-    Rectangle rec;
-    rec.x = 310;
-    rec.y = 680;
-    rec.height = 30;
-    rec.width = 100;
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+
+    // now the paddle
+    paddle.position = {400,650};
+    paddle.size = {100,25};
+    paddle.color = BLACK;
+
+    // Finally init the ball
+
+    ball.position.x = 450;
+    ball.position.y = 620;
+    ball.speed = {0,-5}; // init move up
+    ball.r = 10;
+    ball.c = RED;
+}
+
+
+// Draw the objects onto the canvas: For now just the bricks
+void draw()
+{
+    // Create xy axis
+    DrawLine(450,0,450,720,RED); // y-axis
+    DrawLine(0,360,900,360,DARKBROWN); // x-axis
+
+
+    // draw bricks
+    bool colorSwitch = 0;
+
+    for (int i=0;i<rows;i++)
+    {
+        cout << "drawing bricks" << endl;
+         for (int j=0;j<cols;j++)
+        {
+            Brick brick = bricks[i][j];
+            cout << brick.position.x << ", " << brick.position.y << endl;
+
+            if (colorSwitch == 0 && brick.active)
+            {
+                DrawRectangle(brick.position.x, brick.position.y, brick.size.x,
+                              brick.size.y, RED);
+                colorSwitch = !colorSwitch;
+            }
+            else if (colorSwitch == 1 && brick.active)
+            {
+                DrawRectangle(bricks[i][j].position.x, bricks[i][j].position.y,
+                              bricks[i][j].size.x,bricks[i][j].size.y, DARKBROWN);
+                colorSwitch = !colorSwitch;
+            }
+        }
+        colorSwitch = !colorSwitch;
+    }
+
+    // draw paddle
+    DrawRectangle(paddle.position.x, paddle.position.y, paddle.size.x,paddle.size.y, paddle.color);
+
+    // draw ball
+    DrawCircle(ball.position.x, ball.position.y, ball.r, ball.c);
+
+}
+
+bool inRange(unsigned low, unsigned high, unsigned x)
+{
+    // low = 100,high = 145, 101
+    return (low <= x && x<=high);
+}
+
+bool moveUp = true;
+// I need a function that handles the dynamic content of the game and calls the draw function
+void playGame()
+{
+    if (!(ball.position.y >= screenHeight))
+    {
+        if (IsKeyDown(KEY_LEFT) && paddle.position.x > 10)
+        {
+            paddle.position.x -=5;
+        }
+        else if (IsKeyDown(KEY_RIGHT) && paddle.position.x < 790)
+        {
+            paddle.position.x += 5;
+        }
+
+        ball.position.y += ball.speed.y;
+        ball.position.x += ball.speed.x;
+
+        // check collision of ball and screenHeight
+        if (ball.position.y <= 0)
+        {
+            ball.speed.y *=-1;
+            ball.c = BLACK;
+        }
+
+        // check collision of ball and paddle
+        if (CheckCollisionCircleRec(ball.position,ball.r,
+                                    Rectangle{paddle.position.x,paddle.position.y,paddle.size.x,paddle.size.y}))
+        {
+            ball.speed.y *= -1;
+            int distancePlayer_Ball = ball.position.x - paddle.position.x;
+            ball.speed.x = distancePlayer_Ball/(paddle.size.x/2)*5;
+        }
+
+
+        // check ball collision and screenWidth
+        if (ball.position.x <= 0 ) // hit left wall
+        {
+            ball.speed.x *= -1;
+        }
+        else if (ball.position.x >= 900)
+        {
+            ball.speed.x *= -1;
+        }
+
+        // check collision of ball and any brick
+        for (int i=0;i<rows;i++)
+        {
+            for (int j=0;j<cols;j++)
+            {
+                //   (100,145,paddle.x) && (100,145,paddle.y)
+                if (inRange(bricks[i][j].position.x, bricks[i][j].position.x + 45, ball.position.x + ball.r) &&
+                    (inRange(bricks[i][j].position.y, bricks[i][j].position.y + 45, ball.position.y + ball.r)) && bricks[i][j].active)
+                {
+                    ball.speed.y *= -1;
+                    string x = to_string(bricks[i][j].position.x);
+                    string xTra = to_string(bricks[i][j].position.x + 45);
+                    string pos = x + " , " + xTra;
+                    const char* posPointer = pos.c_str();
+
+                    DrawText(posPointer,200,200,14, BLACK);
+
+                    bricks[i][j].active = false;
+                }
+
+
+            }
+        }
+
+
+
+        draw();
+    }
+    else if (ball.position.y >= screenHeight)
+    {
+        ClearBackground(WHITE);
+        DrawText("Game Over", 370,360, 30, BLACK);
+    }
+
+}
+
+
+
+int main(int argc, char **args)
+{
+    InitWindow(screenWidth, screenHeight,"Arkanoid");
+    SetTargetFPS(60);
+
+    startGame();
+
+    while (!WindowShouldClose())
     {
         ClearBackground(WHITE);
         BeginDrawing();
 
-        int topLeftX = 0;
-        int topLeftY = 0;
-
-        DrawLine(360,10, 360,710, BLACK); // y-axis
-        DrawLine(10,360, 710, 360, BLUE); // x-axis
-
-        // for (int row=0;row<2;row++)
-        // {
-        //     for (int col=0;col<14;col++)
-        //     {
-        //         DrawRectangle(topLeftX,topLeftY,50,50,colors[rand() %(5-0+1)]);
-        //         topLeftX += 50; // move the next rectangle 50px right
-        //     }
-        //     topLeftY +=50; // move to the next row
-        //     topLeftX = 0; // start at the left
-        // }
-
-
-        for (int row=0;row<2;row++)
-        {
-            for (int col=0;col<14;col++)
-            {
-                DrawRectangle(topLeftX,topLeftY,50,50,blockColors[row*14+col] );
-                topLeftX += 50; // move the next rectangle 50px right
-            }
-            topLeftY +=50;
-            topLeftX = 0;
-        }
-
-
-        DrawRectangleRounded(rec, 0.5, 0, RED);
-
-        if (IsKeyDown(KEY_UP))
-        {
-            cout << "Move up \n";
-
-            // Move the rectangle one up;
-            rec.y -= 5;
-            cout << "("<< rec.x << ", " << rec.y << ") \n";
-
-            if (rec.y <= 100)
-            {
-                rec.y = 680;
-                rec.x = 310;
-            }
-        }
-        else if (IsKeyDown(KEY_DOWN))
-        {
-            rec.y += 5;
-            cout << "("<< rec.x << ", " << rec.y << ") \n";
-        }
-        else if (IsKeyDown(KEY_LEFT))
-        {
-            rec.x -= 5;
-            cout << "("<< rec.x << ", " << rec.y << ") \n";
-        }
-        else if (IsKeyDown(KEY_RIGHT))
-        {
-            rec.x += 5;
-            cout << "("<< rec.x << ", " << rec.y << ") \n";
-        }
+        draw();
+        playGame();
         EndDrawing();
     }
 
-    CloseWindow();        // Close window and OpenGL context
-
+    CloseWindow();
 
     return 0;
 }
