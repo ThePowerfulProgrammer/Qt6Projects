62e269f62ea9de9b29e520121a01c39836c07b87
ThePowerfulProgrammer <ashishr0301@gmail.com>
2025-01-15 10:12:25 +0200

    Just added a comment that shows how to create a .db

--- SimpleGuiInteractionWithDB/main.cpp
+++ SimpleGuiInteractionWithDB/main.cpp
@@ -1,10 +1,22 @@
 #include <QApplication>
-
 #include "dialog.h"
+
+
+// QDir currentDir;
+// QString path = currentDir.currentPath() + "/myDB.db";
+// QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+// db.setDatabaseName(path);
+// db.open();
+
+// QSqlQuery query;
+// query.exec("CREATE TABLE Workout"
+//            "id INTERFER PRIMARY KEY"
+//            "exercise VARCHAR(30)");
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     Dialog *dialog = new Dialog;
     dialog->show();
 
